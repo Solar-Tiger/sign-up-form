51fe42fee4dcc2856db3bd7c61f0d906748b5894
David Singleton <davidjsingleton18@gmail.com>
2023-11-25 15:45:52 -0800

    Add name check to prevent names less than 2
    Update phone number check for better error handling and improved regex
    
    Continue handling password error handling

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,14 @@
+const fname = document.getElementById('firstname');
+const lname = document.getElementById('lastname');
 const phoneNumber = document.getElementById('phonenumber');
 const password = document.getElementById('password');
 
+const fnameError = document.querySelector('.firstname-error');
+const lnameError = document.querySelector('.lastname-error');
 const phoneNumberError = document.querySelector('.phonenumber-error');
 const passwordError = document.querySelector('.password-error');
 
-const phoneError = /\d{3}\d{3}\d{4}/;
+const phoneError = /^\d{3}-?\d{3}-?\d{4}-?$/;
 const passError = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\s\w-]).{8,}$/;
 
 const lowercaseLetter = /[a-z]/;
@@ -12,21 +16,43 @@ const uppercaseLetter = /[A-Z]/;
 const numbers = /[0-9]/;
 const specialCharacters = /[^\s\w-]/;
 
-//  Used ChatGPT to come up with the if...else for the ternary operator on the text return check
+// Parameterized the "minLength" by creating minLength instead of hardcoding the 2
 
-function validator(input, error, regex) {
-  if (input.value === '') {
-    input.style.borderColor = '';
-    error.textContent = '';
-  } else if (!regex.test(input.value)) {
-    input.style.borderColor = 'red';
-    error.textContent =
-      regex === phoneError
-        ? '*Invalid phone number'
-        : passwordCheck(input, error);
-  } else {
-    input.style.borderColor = '';
-    error.textContent = '';
+function nameCheck(name, nameError, minLength) {
+  if (name.value === '' || name.value.length > minLength) {
+    name.style.borderColor = '';
+    nameError.classList.remove('error');
+    nameError.textContent = '';
+  } else if (name.value.length <= minLength) {
+    name.style.borderColor = 'red';
+    nameError.classList.add('error');
+    nameError.textContent = "*Name can't be less than 2 characters";
+  }
+}
+
+function pNumberCheck(pNumber, pNumberError, pNumberMaxLength) {
+  console.log(phoneError.test(pNumber.value));
+
+  if (pNumber.value.length === 0 || phoneError.test(pNumber.value)) {
+    pNumber.style.borderColor = '';
+    pNumberError.classList.remove('error');
+    pNumberError.textContent = '';
+  } else if (!phoneError.test(pNumber.value)) {
+    pNumber.style.borderColor = 'red';
+    pNumberError.classList.add('error');
+    pNumberError.textContent = '*Invalid phone number';
+  }
+}
+
+function passwordCheck(pWord, pWordError, regex, pWordMinLength) {
+  if (pWord.value.length === '' || pWord.value.length > pWordMinLength) {
+    pWord.style.borderColor = '';
+    pWordError.classList.remove('error');
+    pWordError.textContent === '';
+  } else if (!regex.test(input.value) && pWord.value.length < pWordMinLength) {
+    pWord.style.borderColor = 'red';
+    pWordError.classList.remove('error');
+    pWordError.textContent === passwordCheck();
   }
 }
 
@@ -48,9 +74,26 @@ function passwordCheck(regexCheck) {
   }
 }
 
+fname.addEventListener('input', () => {
+  nameCheck(fname, fnameError, 2);
+});
+lname.addEventListener('input', () => {
+  nameCheck(lname, lnameError, 2);
+});
 phoneNumber.addEventListener('input', () => {
-  validator(phoneNumber, phoneNumberError, phoneError);
+  pNumberCheck(phoneNumber, phoneNumberError, 10);
 });
 password.addEventListener('input', () => {
-  validator(password, passwordError, passError);
+  passworDCheck(password, passwordError, passError);
+});
+
+const validateButton = document.querySelector('button');
+const formInputs = document.querySelectorAll('input');
+
+validateButton.addEventListener('click', (e) => {
+  formInputs.forEach((input) => {
+    if (input.style.borderColor === 'red') {
+      e.preventDefault();
+    }
+  });
 });
