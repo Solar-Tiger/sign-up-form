2c198e07bc760bd62631a20a67fd97f6ae1f5dd3
David Singleton <davidjsingleton18@gmail.com>
2023-11-28 11:56:11 -0800

    Add function to handle removing errors for easier maintainability

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,7 @@ const phoneNumberError = document.querySelector('.phonenumber-error');
 const passwordError = document.querySelector('.password-error');
 const cPasswordError = document.querySelector('.confirm-password-error');
 
-const phoneError = /^\d{3}-?\d{3}-?\d{4}-?$/;
+const phoneError = /^\d{3}-\d{3}-\d{4}$/;
 const passError = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\s\w-]).{8,}$/;
 
 const lowercaseLetter = /[a-z]/;
@@ -20,25 +20,31 @@ const specialCharacters = /[^\s\w-]/;
 
 // Parameterized the "minLength" by creating minLength instead of hardcoding the 2
 
+// Once the input is valid, removes error
+
+function removeError(inputValue, errorValue) {
+  inputValue.style.borderColor = '';
+  errorValue.classList.remove('error');
+  errorValue.textContent = '';
+}
+
+// Validates if first name or last name are are greater than 3 characters
+
 function nameCheck(name, nameError, minLength) {
-  if (name.value === '' || name.value.length > minLength) {
-    name.style.borderColor = '';
-    nameError.classList.remove('error');
-    nameError.textContent = '';
+  if (name.value.length > minLength) {
+    removeError(name, nameError);
   } else if (name.value.length <= minLength) {
     name.style.borderColor = 'red';
     nameError.classList.add('error');
-    nameError.textContent = "*Name can't be less than 2 characters";
+    nameError.textContent = "*Name can't be less than 3 characters";
   }
 }
 
-function pNumberCheck(pNumber, pNumberError) {
-  console.log(phoneError.test(pNumber.value));
+// Validates if phone number includes 10 digits and two dashes based on phoneError
 
-  if (pNumber.value.length === 0 || phoneError.test(pNumber.value)) {
-    pNumber.style.borderColor = '';
-    pNumberError.classList.remove('error');
-    pNumberError.textContent = '';
+function pNumberCheck(pNumber, pNumberError) {
+  if (phoneError.test(pNumber.value)) {
+    removeError(pNumber, pNumberError);
   } else if (!phoneError.test(pNumber.value)) {
     pNumber.style.borderColor = 'red';
     pNumberError.classList.add('error');
@@ -46,18 +52,19 @@ function pNumberCheck(pNumber, pNumberError) {
   }
 }
 
+// Validates if password matches 8 or more characters as well as includes 1 lowercase character, 1 uppercase character, 1 number and 1 special character
+
 function passwordCheck(pWord, pWordError, regex, pWordMinLength) {
-  console.log(regex.test(pWord.value));
-  if (pWord.value === '' || regex.test(pWord.value)) {
-    pWord.style.borderColor = '';
-    pWordError.classList.remove('error');
-    pWordError.textContent = '';
+  if (regex.test(pWord.value)) {
+    removeError(pWord, pWordError);
   } else if (!regex.test(pWord.value) || pWord.value.length < pWordMinLength) {
     pWord.style.borderColor = 'red';
     pWordError.classList.add('error');
     pWordError.textContent = passwordRequiredCheck(pWord);
   }
 }
+
+// Handles checking for each character to make sure all requirements match
 
 function passwordRequiredCheck(regexCheck) {
   if (!lowercaseLetter.test(regexCheck.value)) {
@@ -79,9 +86,7 @@ function passwordRequiredCheck(regexCheck) {
 
 function confirmPasswordCheck(passOne, passTwo, passTwoError) {
   if (cPassword.value === '' || passOne.value === passTwo.value) {
-    passTwo.style.borderColor = '';
-    passTwoError.classList.remove('error');
-    passTwoError.textContent = '';
+    removeError(passTwo, passTwoError);
   } else if (passOne.value !== passTwo.value) {
     passTwo.style.borderColor = 'red';
     passTwoError.classList.add('error');
