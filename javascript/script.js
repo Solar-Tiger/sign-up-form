f9540d9b1ddf9f45d707a00f665548e185a2c505
David Singleton <davidjsingleton18@gmail.com>
2023-11-30 10:57:58 -0800

    Create prototype function to reduce functions doing repetitive work

--- javascript/script.js
+++ javascript/script.js
@@ -18,14 +18,34 @@ const uppercaseLetter = /[A-Z]/;
 const numbers = /[0-9]/;
 const specialCharacters = /[^\s\w-]/;
 
-// Parameterized the "minLength" by creating minLength instead of hardcoding the 2
+// Parameterized the "minLength" by creating minLength instead of hardcoding the 2, courtesy of chatGPT
 
-// Once the input is valid, removes error
+// Removes error if input properly validates
 
 function removeError(inputValue, errorValue) {
   inputValue.style.borderColor = '';
   errorValue.classList.remove('error');
   errorValue.textContent = '';
+}
+
+function addError(valueInput, errorInput) {
+  valueInput.style.borderColor = 'red';
+  errorInput.classList.add('error');
+  errorInput.textContent = errorType(valueInput);
+}
+
+function errorType(inputName) {
+  if (inputName === fname) {
+    return "*Name can't be less than 3 characters";
+  }
+}
+
+function doesFiveThings(inputOne, inputTwo, inputThree) {
+  if (inputOne === fname && inputOne.value.length <= inputThree) {
+    addError(inputOne, inputTwo);
+  } else {
+    removeError(inputOne, inputTwo);
+  }
 }
 
 // Validates if first name or last name are are greater than 3 characters
@@ -54,10 +74,13 @@ function pNumberCheck(pNumber, pNumberError) {
 
 // Validates if password matches 8 or more characters as well as includes 1 lowercase character, 1 uppercase character, 1 number and 1 special character
 
-function passwordCheck(pWord, pWordError, regex, pWordMinLength) {
-  if (regex.test(pWord.value)) {
+function passwordCheck(pWord, pWordError, pWordMinLength) {
+  if (passError.test(pWord.value)) {
     removeError(pWord, pWordError);
-  } else if (!regex.test(pWord.value) || pWord.value.length < pWordMinLength) {
+  } else if (
+    !passError.test(pWord.value) ||
+    pWord.value.length < pWordMinLength
+  ) {
     pWord.style.borderColor = 'red';
     pWordError.classList.add('error');
     pWordError.textContent = passwordRequiredCheck(pWord);
@@ -84,7 +107,9 @@ function passwordRequiredCheck(regexCheck) {
   }
 }
 
-function confirmPasswordCheck(passOne, passTwo, passTwoError) {
+// Checks to see if both passwords match whether you've entered something into one field, the other or both
+
+function confirmPasswordCheck(passTwo, passTwoError, passOne) {
   if (cPassword.value === '' || passOne.value === passTwo.value) {
     removeError(passTwo, passTwoError);
   } else if (passOne.value !== passTwo.value) {
@@ -104,11 +129,11 @@ phoneNumber.addEventListener('input', () => {
   pNumberCheck(phoneNumber, phoneNumberError);
 });
 password.addEventListener('input', () => {
-  passwordCheck(password, passwordError, passError, 8);
-  confirmPasswordCheck(password, cPassword, cPasswordError);
+  passwordCheck(password, passwordError, 8);
+  confirmPasswordCheck(cPassword, cPasswordError, password);
 });
 cPassword.addEventListener('input', () => {
-  confirmPasswordCheck(password, cPassword, cPasswordError);
+  confirmPasswordCheck(cPassword, cPasswordError, password);
 });
 
 const validateButton = document.querySelector('button');
